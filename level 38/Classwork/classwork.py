7720930c7008bb9f45d64a9fd18703ef9a03fc97
nikolozi12312 <nikawelidze42@gmail.com>
2024-12-24 17:41:51 +0400

    commit

---
+++ level 38/Classwork/classwork.py
@@ -0,0 +1,7 @@
+listn = [i for i in range(1,100)]
+
+listn1 = ["anna","mary","mevludi","nika"] 
+listn2 = ["#" + i.replace("a","") for i in listn1]
+
+listn3 = [i for i in range(1,100,2)]
+print(listn2)
